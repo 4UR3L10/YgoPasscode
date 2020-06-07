cf6c375efb75c70446af327fe099f82ced75cf92
4UR3L10 <aureliomartinezdros@outlook.com>
2020-06-07 17:27:41 -0400

    Initial
    Finished

---
+++ ExtractCode/ExtractCode/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ExtractCode
+{
+    partial class FormGetPasscode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonGetCode = new System.Windows.Forms.Button();
+            this.labelPasscodes = new System.Windows.Forms.Label();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxPasscode = new System.Windows.Forms.TextBox();
+            this.listBoxPasscode = new System.Windows.Forms.ListBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(1346, 873);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(126, 43);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonGetCode
+            // 
+            this.buttonGetCode.Location = new System.Drawing.Point(61, 873);
+            this.buttonGetCode.Name = "buttonGetCode";
+            this.buttonGetCode.Size = new System.Drawing.Size(126, 43);
+            this.buttonGetCode.TabIndex = 1;
+            this.buttonGetCode.Text = "Get Code";
+            this.buttonGetCode.UseVisualStyleBackColor = true;
+            this.buttonGetCode.Click += new System.EventHandler(this.buttonGetCode_Click);
+            // 
+            // labelPasscodes
+            // 
+            this.labelPasscodes.AutoSize = true;
+            this.labelPasscodes.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPasscodes.Location = new System.Drawing.Point(54, 26);
+            this.labelPasscodes.Name = "labelPasscodes";
+            this.labelPasscodes.Size = new System.Drawing.Size(213, 42);
+            this.labelPasscodes.TabIndex = 2;
+            this.labelPasscodes.Text = "Passcodes:";
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.FileName = "OpenFile";
+            // 
+            // textBoxPasscode
+            // 
+            this.textBoxPasscode.Location = new System.Drawing.Point(61, 85);
+            this.textBoxPasscode.Multiline = true;
+            this.textBoxPasscode.Name = "textBoxPasscode";
+            this.textBoxPasscode.Size = new System.Drawing.Size(1387, 314);
+            this.textBoxPasscode.TabIndex = 4;
+            // 
+            // listBoxPasscode
+            // 
+            this.listBoxPasscode.FormattingEnabled = true;
+            this.listBoxPasscode.ItemHeight = 25;
+            this.listBoxPasscode.Location = new System.Drawing.Point(61, 443);
+            this.listBoxPasscode.Name = "listBoxPasscode";
+            this.listBoxPasscode.Size = new System.Drawing.Size(1387, 354);
+            this.listBoxPasscode.TabIndex = 5;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(692, 873);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(126, 43);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // FormGetPasscode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 934);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listBoxPasscode);
+            this.Controls.Add(this.textBoxPasscode);
+            this.Controls.Add(this.labelPasscodes);
+            this.Controls.Add(this.buttonGetCode);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "FormGetPasscode";
+            this.Text = "Get Passcode";
+            this.Load += new System.EventHandler(this.FormGetPasscode_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonGetCode;
+        private System.Windows.Forms.Label labelPasscodes;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+        private System.Windows.Forms.TextBox textBoxPasscode;
+        private System.Windows.Forms.ListBox listBoxPasscode;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ ExtractCode/ExtractCode/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtractCode
+{
+    public partial class FormGetPasscode : Form
+    {
+        public FormGetPasscode()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGetPasscode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGetCode_Click(object sender, EventArgs e)
+        {
+            // Global Declarations.
+            StreamReader inputFile;
+
+            // Try to Open a file, catch errors if there are.
+            try
+            {
+                // Open the file with explorer.
+                if (OpenFile.ShowDialog() == DialogResult.OK)
+                {
+                    // Open the file and get a StreamReader object.               
+                    inputFile = File.OpenText(OpenFile.FileName);
+
+                    // Filetype Validation.
+                    if (!OpenFile.ToString().Contains(".txt"))
+                    {
+                        MessageBox.Show("That is not a (.txt) file!!!");
+                    }
+                    else
+                    {
+                        // Declare variables to hold all the passscodes.
+                        string sPasscode = "";
+                        string sTempPasscode = "";
+                        string sTempTwoPasscode = "";
+
+                        // Read the file's contents.
+                        while (!inputFile.EndOfStream)
+                        {                          
+                            // Get lines.
+                            sTempPasscode = inputFile.ReadLine() + "\n";
+
+                            // Check if the line have //ygoprodeck.com/pics/ substring.
+                            Boolean temp = false;
+                            temp = sTempPasscode.Contains("src=\"https://ygoprodeck.com/pics/");
+
+                            // If the line contains it.
+                            if (temp)
+                            {
+                                // Get this substring in this format /pics/########.
+                                sTempTwoPasscode = sTempPasscode.Substring(sTempPasscode.LastIndexOf("/pics/"), 14) + "\n";
+
+                                // Getting the first char after /pics/#.
+                                char tempChar = sTempTwoPasscode[7];
+
+                                // If that char is a number.
+                                if ((int)Char.GetNumericValue(tempChar) >= 0 && (int)Char.GetNumericValue(tempChar) <= 9)
+                                {
+                                    // Get the whole 8-digit number.
+                                    sPasscode = sTempTwoPasscode.Substring(6,8) + "\n";
+
+                                    // Add the Passcode to the ListBox.
+                                    listBoxPasscode.Items.Add(sPasscode);
+                                }
+                            }      
+                        }
+                    }
+
+                    // Copying all values from the listbox to the textBox.
+                    String text = "";
+                    foreach (var item in listBoxPasscode.Items)
+                    {
+                        text += item.ToString(); // /n to print each item on new line or you omit /n to print text on same line
+                    }
+                    textBoxPasscode.Text = text;
+
+                    // Close the file.
+                    inputFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("You clicked the Cancel button.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display an error message.
+                MessageBox.Show(ex.Message);
+            }                              
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            // Close the Program.
+            this.Close();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Clear anything currently in the ListBox.
+            listBoxPasscode.Items.Clear();
+            textBoxPasscode.Text = "";
+        }
+    }
+}
